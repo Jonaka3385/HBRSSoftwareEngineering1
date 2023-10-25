b68c296b46a2df72d90bc0436b6662772ed46e00
Jonaka <j.kawohl@icloud.com>
2023-10-25 15:47:04 +0200

    Commit

--- src/uebung2/Container.java
+++ src/uebung2/Container.java
@@ -35,7 +35,6 @@ public class Container {
     }
 
     public void dump() {
-        Member current;
         for (Member member : list) {
             System.out.println(member.toString());
         }
