c51325a15e7799c31df76772ec37d6f7e9c270ee
Jonaka <j.kawohl@icloud.com>
2023-11-02 13:28:06 +0100

    Commit

---
+++ src/uebung3/persistence/Client.java
@@ -0,0 +1,10 @@
+package uebung3.persistence;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Container container = new Container();
+        // Zweites Objekt?
+    }
+
+}
