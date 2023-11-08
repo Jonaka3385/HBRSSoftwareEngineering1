5d9bf8a6280bcdbc28cf60de280c6cc71050ec26
Jonaka <j.kawohl@icloud.com>
2023-11-08 15:47:08 +0100

    Commit

--- src/uebung3/MemberView.java
+++ src/uebung3/MemberView.java
@@ -5,8 +5,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class MemberView {
-    public void dump(@NotNull List<Member> liste){
+    public void dump(@NotNull List<Member> list){
         System.out.println("Ausgabe aller Member-Objekte: ");
-        liste.parallelStream().forEach(System.out::println);
+        list.parallelStream().forEach(System.out::println);
     }
 }
